20362b73fadd8c4a90b36d4898ee073774600504
WTanardi <william.tanardi-2021@fst.unair.ac.id>
2023-12-07 18:17:52 +0700

    updated paths

---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+
+export default function CourseCard({ courseData }) {
+  return (
+    <div className='flex flex-col bg-white border-[1px] border-slate-200 w-[300px] flex-none'>
+      {/* Card img */}
+      <div
+        className='bg-cover bg-center w-full h-96 self-center'
+        style={{
+          backgroundImage: `url(${courseData.img})`,
+        }}
+      ></div>
+
+      <div className='gap-2 flex flex-col px-6 py-4 justify-between h-1/2'>
+        {/* Title & Desc */}
+        <div className='flex flex-col gap-2'>
+          <p className='text-md opacity-50'>Step {courseData.number}</p>
+          <p className='text-lg font-medium'>{courseData.title}</p>
+          <p className='text-sm'>{courseData.desc}</p>
+        </div>
+
+        {/* Stats */}
+        <div className='flex justify-between text-xs gap-2'>
+          <div className='flex flex-col gap-1'>
+            {/* Rating */}
+            <div className='flex items-center gap-1'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                stroke-width='2'
+                className='w-4'
+              >
+                <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2' />
+              </svg>
+              {courseData.rating}
+            </div>
+
+            {/* Level */}
+            <div className='flex items-center gap-1'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                stroke-width='2'
+                className='w-4'
+              >
+                <circle
+                  cx='12'
+                  cy='8'
+                  r='6'
+                />
+                <path d='M15.477 12.89 17 22l-5-3-5 3 1.523-9.11' />
+              </svg>
+              {courseData.level} Level
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            {/* Hours */}
+            <div className='flex items-center gap-1'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                stroke-width='2'
+                className='w-4'
+              >
+                <circle
+                  cx='12'
+                  cy='12'
+                  r='10'
+                />
+                <polyline points='12 6 12 12 16 14' />
+              </svg>
+              {courseData.hours} Hours
+            </div>
+
+            {/* Students */}
+            <div className='flex items-center gap-1'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                stroke-width='2'
+                className='w-4'
+              >
+                <path d='M18 21a8 8 0 0 0-16 0' />
+                <circle
+                  cx='10'
+                  cy='8'
+                  r='5'
+                />
+                <path d='M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3' />
+              </svg>
+              {courseData.students}k Students
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Paths.jsx
+++ src/components/Paths.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import CourseCard from './CourseCard';
 
 export default function Paths() {
   const cardData = [
@@ -9,9 +10,9 @@ export default function Paths() {
       content:
         'Dive into the world of stunning user interfaces, interactive web apps, and pixel-perfect design',
       img: '/fecard.png',
-      classes: 6,
-      students: 251,
-      desc: 'Follow a curriculum designed by the best of the best, from industrial specialists to leading educators, ensuring you receive unparalleled expertise and guidance on your learning journey.',
+      classes: 8,
+      students: 320,
+      desc: 'Immerse yourself in a comprehensive curriculum crafted by industry leaders and educators. Receive unparalleled expertise and guidance on your journey to becoming a front-end developer.',
     },
     {
       id: 'bedev',
@@ -19,9 +20,9 @@ export default function Paths() {
       content:
         'Explore databases, server logic, and scalability to create robust and efficient web applications.',
       img: '/becard.png',
-      classes: 6,
-      students: 251,
-      desc: 'Follow a curriculum designed by the best of the best, from industrial specialists to leading educators, ensuring you receive unparalleled expertise and guidance on your learning journey.',
+      classes: 7,
+      students: 280,
+      desc: 'Master the intricacies of back-end development with a curriculum designed by industry specialists. Gain expertise in creating scalable and efficient web applications.',
     },
     {
       id: 'apidev',
@@ -30,8 +31,8 @@ export default function Paths() {
         'Learn how to create and consume APIs, enabling your applications to communicate and integrate seamlessly',
       img: '/apicard.png',
       classes: 6,
-      students: 251,
-      desc: 'Follow a curriculum designed by the best of the best, from industrial specialists to leading educators, ensuring you receive unparalleled expertise and guidance on your learning journey.',
+      students: 240,
+      desc: 'Delve into the world of API development with a curriculum crafted by leading educators and industry experts. Acquire the skills to enable seamless communication and integration in your applications.',
     },
     {
       id: 'gitdev',
@@ -39,9 +40,62 @@ export default function Paths() {
       content:
         'Learn version control, collaboration, and best practices. Streamline your development workflow and work seamlessly in teams',
       img: '/gitcard.png',
-      classes: 6,
-      students: 251,
-      desc: 'Follow a curriculum designed by the best of the best, from industrial specialists to leading educators, ensuring you receive unparalleled expertise and guidance on your learning journey.',
+      classes: 5,
+      students: 200,
+      desc: 'Master Git and GitHub with a curriculum designed by the best in the field. Streamline your development workflow, practice version control, and collaborate effectively in team environments.',
+    },
+  ];
+
+  const courseData = [
+    {
+      number: 1,
+      title: 'Learn the basics of web programming',
+      desc: 'Learn HTML and CSS, the basic building blocks of the web, and start crafting stunning and responsive websites that leave a lasting impression.',
+      img: '/courseprev1.png',
+      rating: 4.87,
+      hours: 45,
+      level: 'Beginner',
+      students: 141.4,
+    },
+    {
+      number: 2,
+      title: 'Introduction to JavaScript',
+      desc: 'Explore the fundamentals of JavaScript programming language and enhance your web development skills by adding dynamic and interactive features to your websites.',
+      img: '/courseprev2.png',
+      rating: 4.65,
+      hours: 30,
+      level: 'Intermediate',
+      students: 89.2,
+    },
+    {
+      number: 3,
+      title: 'Responsive Web Design with Bootstrap',
+      desc: 'Master the art of responsive web design using the Bootstrap framework. Create visually appealing and mobile-friendly websites with ease.',
+      img: '/courseprev3.png',
+      rating: 4.92,
+      hours: 50,
+      level: 'Intermediate',
+      students: 112.8,
+    },
+    {
+      number: 4,
+      title: 'Node.js for Backend Development',
+      desc: 'Dive into server-side development with Node.js. Learn how to build scalable and efficient backend applications using JavaScript.',
+      img: '/courseprev4.png',
+      rating: 4.75,
+      hours: 40,
+      level: 'Advanced',
+      students: 97.5,
+    },
+    {
+      number: 5,
+      title: 'React.js: Building User Interfaces',
+      desc: 'Build interactive and dynamic user interfaces with React.js. Explore the component-based architecture and create modern, efficient web applications.',
+      img: '/courseprev5.png',
+      rating: 4.88,
+      hours: 55,
+      level: 'Advanced',
+      students: 125.6,
     },
   ];
 
@@ -74,8 +128,9 @@ export default function Paths() {
           <TabsContent
             value={e.id}
             key={i}
+            className='w-full'
           >
-            <div className='flex p-8 m-4 bg-slate-100'>
+            <div className='flex max-lg:flex-wrap p-8 m-4 bg-slate-100'>
               <div className='text-left gap-2 flex flex-col'>
                 {/* Course title */}
                 <p className='text-2xl font-medium mb-1'> {e.title} </p>
@@ -118,12 +173,21 @@ export default function Paths() {
                 </div>
 
                 {/* Line */}
-                <div className='w-full border-[1px] border-slate-200'></div>
+                <div className='w-full border-2 border-slate-200'></div>
 
                 {/* Course description */}
                 <div className='py-6 text-sm'>{e.desc}</div>
               </div>
-              <div>{/* Course cards */}</div>
+
+              {/* Course cards */}
+              <div className='overflow-x-scroll flex gap-2 justify-start'>
+                {courseData.map((e, i) => (
+                  <CourseCard
+                    courseData={e}
+                    key={i}
+                  />
+                ))}
+              </div>
             </div>
           </TabsContent>
         ))}
