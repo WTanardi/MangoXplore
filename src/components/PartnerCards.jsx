c55bd861dee594e492e824f062631a60adb35068
WTanardi <william.tanardi-2021@fst.unair.ac.id>
2023-12-07 19:26:01 +0700

    added testimonial section

---
+++ src/components/PartnerCards.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function PartnerCards() {
+  const partners = [
+    '/google.png',
+    '/microsoft.png',
+    '/aws.png',
+    '/ibm.png',
+    '/lenovo.png',
+    '/intel.png',
+    '/kominfo.png',
+    '/bangkit.png',
+    '/line.png',
+    '/samsung.png',
+    '/kemenparekraf.png',
+    '/mbkm.png',
+  ];
+  return (
+    <div className='flex flex-wrap justify-evenly gap-4 lg:gap-8'>
+      {partners.map((e, i) => (
+        <div
+          className='lg:h-24 lg:w-64 h-16 w-40 border-[1px] border-black flex items-center justify-center'
+          key={i}
+        >
+          <img
+            src={e}
+            alt=''
+            className='w-20'
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/components/TestimonialCards.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default function TestimonialCards() {
+  const cards = [
+    {
+      img: '/jessica.png',
+      name: 'Jessica S.',
+      position: 'Aspiring Web Developer',
+      desc: 'Enrolling in the web development courses at MangoXplore was a game-changer for me. The instructors provided clear explanations and hands-on practice, making learning HTML, CSS, and JavaScript a breeze. Now, I can confidently create beautiful websites from scratch.',
+    },
+    {
+      img: '/alex.png',
+      name: 'Alex M.',
+      position: 'Web Enthusiast turned Developer',
+      desc: 'Studying web programming with MangoXplore was a fantastic experience. The curriculum was well-structured, and the instructors were patient and knowledgeable. I started with zero coding experience, and now I can build and style web pages with ease. MangoXplore truly made learning HTML and CSS enjoyable and accessible.',
+    },
+  ];
+
+  return (
+    <div className='flex flex-col gap-4'>
+      {cards.map((e, i) => (
+        <div
+          key={i}
+          className='p-6 bg-stone-100 mx-8 rounded shadow-xl'
+        >
+          {/* Text */}
+          <div className='flex gap-2 flex-col'>
+            <div className='flex gap-4'>
+              {/* Image */}
+              <div
+                className='text-center justify-center p-6 text-white max-w-1/3 aspect-square bg-cover bg-center'
+                style={{
+                  backgroundImage: `url(${e.img})`,
+                }}
+              ></div>
+              <div>
+                <p className='text-3xl font-medium'> {e.name} </p>
+                <p className='text-sm '> {e.position} </p>
+              </div>
+            </div>
+            <p className='text-xs text-justify'> {e.desc} </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
